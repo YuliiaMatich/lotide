53f433f9588456f61545e84d3e74fbd93cd41584
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-04 16:28:12 -0400

    added chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,17 @@
-// assertEqual test code
+// const assertEqual = require('../assertEqual');
+
+// assertEqual("bootcamp", "Lighthouse Labs");
+// assertEqual(2, 2);
+
+
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual("bootcamp", "Lighthouse Labs");
-assertEqual(2, 2);
-
+describe("#assertEqual", () => {
+  it("returns 'bootcamp' for 'bootcamp'", () => {
+    assert.strictEqual(assertEqual('bootcamp', 'Lighthose labs'), false);
+  });
+  it("returns true for '2'", () => {
+    assert.strictEqual(assertEqual('2', '2'), true); 
+  });
+});
