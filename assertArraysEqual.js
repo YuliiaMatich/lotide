efd9eb0834261ddc359cf35b67015d749d2ba35d
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-04 16:54:30 -0400

    added chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,8 +2,10 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else (console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+    return true;
+  } else {
+    return false;
+  };
 };
 
 module.exports = assertArraysEqual;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,18 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+// assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+
+
+const assert = require('chai').assert;
+const assertArraysEqual   = require('../assertArraysEqual');
+
+describe("#assertArraysEqual", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns returns false for ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.strictEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); 
+  });
+});
