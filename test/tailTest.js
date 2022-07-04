e2b96479e363135507a859a11e813a278ee465ee
YuliiaMatich <yuliia.matich@gmail.com>
2022-07-04 17:33:36 -0400

    added chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(words).length, 1);
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words).length, 1);
+
+describe("#tail", () => {
+  it("returns true for ['Yo Yo', 'Lighthouse', 'Labs'] and ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns returns true for [1, 2, 3], [2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]); 
+  });
+});
